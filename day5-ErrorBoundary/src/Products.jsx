72a07ef0aa35021030b63d7108beb2557767b0c4
Johnpaul P <162032438+itsjohnpaul@users.noreply.github.com>
2026-01-19 15:12:28 +0530

    Update Products.jsx

--- day5-ErrorBoundary/src/Products.jsx
+++ day5-ErrorBoundary/src/Products.jsx
@@ -1,6 +1,7 @@
 import "./Products.css";
-
+import {useState} from "react";
 function Products({ products }) {
+  const [btn,setInc]=useState(0);
   return (
     <div className="user">
       <h2 className="title">Users List</h2>
@@ -10,8 +11,14 @@ function Products({ products }) {
          <p>NAME: {user.name}</p>
         </div>
       ))}
+      <p>Count:{btn}</p>
+      <button onClick={()=>setInc(btn+1)}>Click</button>
+      if(btn==5){
+        throw new Error("You have reached the limit !");
+      }
     </div>
   );
 }
 
 export default Products;
+
