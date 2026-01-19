f0976b11dc373ed3f9567f5770ea6e0b07e0d012
Johnpaul P <162032438+itsjohnpaul@users.noreply.github.com>
2026-01-19 15:24:20 +0530

    Update Products.jsx

--- day5-ErrorBoundary/src/Products.jsx
+++ day5-ErrorBoundary/src/Products.jsx
@@ -1,21 +1,23 @@
 import "./Products.css";
-import {useState} from "react";
+import { useState } from "react";
 function Products({ products }) {
-  const [btn,setInc]=useState(0);
+  const [btn, setInc] = useState(0);
+  if (btn == 5) {
+    throw new Error("Something went Wrong !");
+  }
   return (
     <div className="user">
       <h2 className="title">Users List</h2>
+   <center>  <button onClick={() => setInc(btn + 1)}>Click</button>  </center>
+      <p style={{textAlign:"center"}}>Count:{btn}</p>
+
       {products.map(user => (
         <div key={user.id} className="card">
-         <p>ID: {user.id}</p>
-         <p>NAME: {user.name}</p>
+          <p>ID: {user.id}</p>
+          <p>NAME: {user.name}</p>
         </div>
       ))}
-      <p>Count:{btn}</p>
-      <button onClick={()=>setInc(btn+1)}>Click</button>
-      if(btn==5){
-        throw new Error("You have reached the limit !");
-      }
+
     </div>
   );
 }
